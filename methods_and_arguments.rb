851bfc586a18771d517e29c3ff7a54e836034d49
Davis Brown <cooliomoded@gmail.com>
2021-03-16 20:13:59 +0000

    Automatically backed up by Learn

---
+++ methods_and_arguments.rb
@@ -0,0 +1,9 @@
+def greeting_a_person(name, language)
+  puts "Hello, #{name}. We heard you  are a great #{language} programmer."
+end
+
+greeting_a_person("Davis", "Ruby")
+
+greeting_a_person("Maria", "Javascript")
+
+greeting_a_person("Steven", "Elixir")
